f9ed468b2f8c176e7636aa75162da11381c80b95
Sanmar Simon <sanmarsimon@gmail.com>
2020-11-27 19:11:24 -0500

    Init commit

---
+++ Jeu.h
@@ -0,0 +1,13 @@
+#pragma once
+#ifndef JEU_H
+#define JEU_H
+
+#include <iostream>
+
+using namespace std;
+
+class Jeu
+{
+};
+
+#endif JEU_H
---
+++ Lexique.h
@@ -0,0 +1,13 @@
+#pragma once
+#ifndef LEXIQUE_H
+#define LEXIQUE_H
+#include <iostream>
+
+
+using namespace std;
+
+class Lexique
+{
+};
+
+#endif LEXIQUE_H
